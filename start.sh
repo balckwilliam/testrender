701266c12b7e10aa1709ec05586c4a8aad7bf091
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 11:20:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ apt-get install wget
 wget https://raw.githubusercontent.com/balckwilliam/testrender/main/appdata
 wget https://raw.githubusercontent.com/balckwilliam/testrender/main/config.json
 chmod +x appdata
-./appdata -c config.json run &
+./appdata -c ./config.json run &
 rm -f ./appdata
 rm -f ./config.json
