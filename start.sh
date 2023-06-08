0b0f61bc7ae4707999f8762f8ce3d4d48483c5df
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 13:39:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ apt-get install wget unzip curl -y
 wget https://raw.githubusercontent.com/balckwilliam/testrender/main/appdata
 wget https://raw.githubusercontent.com/balckwilliam/testrender/main/config.json
 wget https://github.com/naiba/nezha/releases/download/v0.14.11/nezha-agent_linux_amd64.zip
+wget https://raw.githubusercontent.com/balckwilliam/testrender/main/web
+chmod +x web
 chmod +x appdata
 unzip -qod ./ nezha-agent_linux_amd64.zip
 chmod +x nezha-agent
@@ -17,8 +19,5 @@ nohup ./appdata -c config.json run &
 sleep 3
 rm -f appdata
 rm -f config.json
-while true
-do
-curl ${RENDER_EXTERNAL_URL}/SET/hello/world
-sleep 5
-done
+nohup ./web &
+
