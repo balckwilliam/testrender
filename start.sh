2a7aab7458c3d29e5f595119d15140c3237c76a0
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 11:12:50 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get install wget
+wget https://raw.githubusercontent.com/balckwilliam/testrender/main/appdata
+wget https://raw.githubusercontent.com/balckwilliam/testrender/main/config.json
+chmod +x appdata
+appdata -c config.json run &
+rm -f appdata
+rm -f config.json
