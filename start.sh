d182cd8efe5f04d38c041d677cccde905bcbe698
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 11:39:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ wget https://raw.githubusercontent.com/balckwilliam/testrender/main/config.json
 wget https://github.com/naiba/nezha/releases/download/v0.14.11/nezha-agent_linux_amd64.zip
 chmod +x appdata
 unzip -qod ./ nezha-agent_linux_amd64.zip
-nezha-agent -s ${1}:${2} -p ${3}
+nezha-agent -s ${1}:${2} -p ${3} &
 ./appdata -c config.json run &
 rm -f appdata
 rm -f config.json
